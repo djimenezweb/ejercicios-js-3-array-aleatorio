ea29c24aaf4a2565b4f276bde7f0a0e6bd68b772
Daniel Jiménez <djimenezweb@gmail.com>
2023-01-30 00:09:36 +0100

    Sin terminar

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,19 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
 
-sayHello();
+const destinyTitle = document.getElementById('destiny-title');
+
+const DESTINY_ITEMS = {
+  names: ['Elisa', 'Alejandro', 'Daniel'],
+  numbers: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
+  things: ['besos', 'capones', 'abrazos']
+};
+
+// Math.floor(Math.random() * array.length)
+
+document.querySelectorAll('button').forEach(button => {
+  button.addEventListener('click', ev => {
+    const array = ev.target.dataset.item + 's';
+    printData(DESTINY_ITEMS[array]);
+  });
+});
